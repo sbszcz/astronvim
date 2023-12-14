fbdc782dde479553f4f3ff7978df90048f3c7e92
Sebastian Barszczewski <sebastian.barszczewski@gmail.com>
2023-12-14 10:42:13 +0100

    hide tabs and enable file name in status line

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -6,7 +6,7 @@ return {
       hl = { fg = "fg", bg = "bg" },
       status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
       status.component.git_branch(),
-      status.component.file_info { filetype = {}, filename = false, file_modified = false },
+      status.component.file_info { filetype = false, filename = {}, file_modified = { padding = { left = 1 } } },
       status.component.git_diff(),
       status.component.diagnostics(),
       status.component.fill(),
